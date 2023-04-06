36926ff48a8beca011f731816e29dceff02d1afc
zhangtao <2175216104@qq.com>
2023-04-06 14:44:56 +0800

    first commit

---
+++ src/main/java/com/example/gittest/GitTestApplication.java
@@ -0,0 +1,18 @@
+package com.example.gittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitTestApplication.class, args);
+
+        System.out.println("11111111");
+        System.out.println("22222222");
+        System.out.println("33333333");
+        System.out.println("44444444");
+    }
+
+}
